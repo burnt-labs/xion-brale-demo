3d76438f033abbf32fa202d147e57b2d209b305c
Adrian Thompson <adrian.delroy.thompson@gmail.com>
2026-03-24 12:05:47 -0500

    Add .gitignore and remove build artifacts from tracking

--- xion-ios/build/Build/Intermediates.noindex/UncommonCrypto.build/Debug-iphonesimulator/CUncommonCrypto.build/Objects-normal/arm64/hmac.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/adrianthompson/Documents/35\ -\ XION/02\ -\ Projects/29\ -\ House\ Money/apps/xion-ios/vendor/uncommon-crypto/Sources/CUncommonCrypto/hmac.c \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include/c_standard_library.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/17/include/module.modulemap \
-  /Users/adrianthompson/Documents/35\ -\ XION/02\ -\ Projects/29\ -\ House\ Money/apps/xion-ios/vendor/uncommon-crypto/Sources/CUncommonCrypto/include/hmac.h \
-  /Users/adrianthompson/Documents/35\ -\ XION/02\ -\ Projects/29\ -\ House\ Money/apps/xion-ios/vendor/uncommon-crypto/Sources/CUncommonCrypto/include/sha2.h \
-  /Users/adrianthompson/Documents/35\ -\ XION/02\ -\ Projects/29\ -\ House\ Money/apps/xion-ios/vendor/uncommon-crypto/Sources/CUncommonCrypto/include/options.h \
-  /Users/adrianthompson/Documents/35\ -\ XION/02\ -\ Projects/29\ -\ House\ Money/apps/xion-ios/vendor/uncommon-crypto/Sources/CUncommonCrypto/memzero.h
